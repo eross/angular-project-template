6ea6b7d80e22be7c2204dbdc4a28c919bbb62a2e
Eric Ross <epr@evross.com>
2017-09-15 13:06:13 -0700

    Final Section 3 code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
 }
-
-//console.debug("hello");
-//console.debug("jQuery "+ (jQuery ? $().jquery : "NOT") +" loaded");
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 
-@Component(
-    {
-        selector: 'app-header',
-        templateUrl: './header.component.html'
-    }
-)
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
 export class HeaderComponent {
 
 }
